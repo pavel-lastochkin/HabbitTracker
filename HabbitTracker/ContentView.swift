8a2228527121e320fc6b8a4e918d9944e091c962
LaSStiK <LaSStiK@ya.ru>
2026-05-31 18:56:08 +0300

    Initial Commit

---
+++ HabbitTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HabbitTracker
+//
+//  Created by Pavel Lastochkin on 31.05.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HabbitTracker/HabbitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabbitTrackerApp.swift
+//  HabbitTracker
+//
+//  Created by Pavel Lastochkin on 31.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct HabbitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
